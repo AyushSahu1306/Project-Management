c6b1529df10c5479fb37d3d320ca80e7ce1e5e7c
Ayush Sahu <sahuayush0613@gmail.com>
2025-09-12 19:33:27 +0530

    Add Navbar component and integrate it into DashboardWrapper

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Navbar from '@/app/(components)/Navbar'
 
 
 const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
@@ -9,7 +9,7 @@ const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
         sidebar
         <main className='dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64'>
             {/* navbar */}
-            
+            <Navbar/>
             {children}
         </main>
     </div>
